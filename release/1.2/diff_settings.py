e22390f68a87c647b9651ff8c383be28c6f07b8c
Fabio Mierlo <fabio@n42.org>
2016-06-16 01:10:56 -0300

    release: Start 1.2

---
+++ release/1.2/diff_settings.py
@@ -0,0 +1,9 @@
+DATABASES = {'default': {'ENGINE': 'django.db.backends.', 'NAME': '', 'HOST': '', 'USER': '', 'PASSWORD': '', 'PORT': ''}}
+DEBUG = True
+INSTALLED_APPS = ['django.contrib.auth', 'django.contrib.contenttypes', 'django.contrib.sessions', 'django.contrib.sites', 'django.contrib.messages']
+ROOT_URLCONF = 'project.urls'  ###
+SECRET_KEY = '01234567890123456789012345678901234567890123456789'
+SETTINGS_MODULE = 'project.settings'  ###
+SITE_ID = 1  ###
+TEMPLATE_DEBUG = True
+USE_L10N = True
